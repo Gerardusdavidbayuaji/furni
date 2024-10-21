46d34bbe7fa624ad4c75b99896e623472bdfef3b
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2024-10-21 21:21:42 +0700

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const Layout = () => {
+  return (
+    <div>
+      <h1>testing button</h1>
+      <Button className="bg-red-500">click me</Button>
+    </div>
+  );
+};
+
+export default Layout;
